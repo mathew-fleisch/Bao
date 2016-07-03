3919a2d33764f7187ad82b2ee86eb8169407cf9c
Mathew Fleisch <mfleisch@Mac-Bizzle.local>
2016-07-02 18:16:35 -0700

    Fixed logic errors and introduced a delay between moves.

--- js/script.js
+++ js/script.js
@@ -8,67 +8,44 @@ $(document).on("click", ".bin", function() {
 	move(position);
 });
 function move(position){
-	console.log("Position:",position);
-	if(is_valid_move(position, true)) { 
+	console.log("Position: ("+position.pos+")",position);
+	if(position.pos > 16) { position.pos = position.pos - 16; }
+	if(is_valid_move(position, true) && position.pos !== bank) { 
 		// console.log("beans:"+position.beans);
+		var beans = $(position.target+" > .bean").length;
 		$(position.target+" > .bean").remove();
 		var i = 0;
-		move_bean(i, position);
-		// for(var i = 1; i <= position.beans; i++) { 
-		// 	var new_pos = ((position.pos+i) > 16 ? (position.pos-16+i) : (position.pos+i));
-		// 	var new_target = position.target.replace(/p_.*/, "p_"+new_pos);
-		// 	$(new_target).append('<div class="bean">&nbsp;</div>');
-		// 	var new_position = { 
-		// 		"player":position.player,
-		// 		"pos":(position.pos+i),
-		// 		"beans":$(new_target+" > .bean").length,
-		// 		"target":new_target
-		// 	};
-
-		// 	if(i == position.beans) {
-		// 		var final_bin = $(new_target+" > .bean").length;
-		// 		console.log("Final Bin: "+final_bin);
-		// 		// console.log(new_position);
-		// 		// console.log("Next -> "+is_valid_move(new_position));
-		// 		// console.log("$(\""+new_target+"\").click();");
-		// 		if(is_valid_move(new_position)) { $(new_target).click(); }
-		// 	}
-		// }
+		move_bean(i, beans, position);
 	}
 }
-function move_bean(i, position) {
+function move_bean(i, beans, position) {
+	// alert("move_bean");
 	var track = i;
+	var speed = 300;
 	setTimeout(function() {
 		i++;
-		console.log("move_bean(track:"+track+",i:"+i+",position:",position);
-		if(i <= position.beans) {
+		console.log("move_bean(track:"+track+",i:"+i+",beans:"+beans+",position:",position);
+		if(i <= beans) {
 			var new_pos = ((position.pos+1) > 16 ? (position.pos-16+1) : (position.pos+1));
 			var new_target = position.target.replace(/p_.*/, "p_"+new_pos);
 			$(new_target).append('<div class="bean">&nbsp;</div>');
 			var new_position = { 
 				"player":position.player,
 				"pos":(position.pos+1),
-				"beans":$(new_target+" > .bean").length,
+				"beans":beans,
 				"target":new_target
 			};
 			console.log("Target: "+new_target);
 			console.log($(new_target).html());
-			console.log("Beans: "+$(new_target+" > .bean").length);
+			console.log("Beans: "+beans);
 
-			if((i) == position.beans) {
+			if(is_valid_move(new_position, false)) { move_bean(i, beans, new_position); }
+		} else { 
 				var final_bin = $(new_target+" > .bean").length;
 				console.log("Final Bin: "+final_bin);
-				// console.log(new_position);
-				// console.log("Next -> "+is_valid_move(new_position));
-				// console.log("$(\""+new_target+"\").click();");
-				// if(is_valid_move(new_position)) { $(new_target).click(); }
-				// move(new_position);
 				if(is_valid_move(position, true)) { move(position); }
-
-			}
-			if(is_valid_move(new_position, false)) { move_bean((i), new_position); }
 		}
-	}, 1000);
+	}, speed);
 }
 function is_valid_move(position, last_move=false) {
 	console.log("is_valid_move()",position);
