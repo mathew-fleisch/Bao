74737d107b9ba3c0594d89ba0ece352c4fae9603
Mathew Fleisch <mfleisch@Mac-Bizzle.local>
2016-07-06 18:01:08 -0700

    Bug fixes for cramping. Added a stats section and some simple controls for speed and to break out of the loop.

--- index.php
+++ index.php
@@ -25,41 +25,67 @@ var ply_pos = <?php echo $ply_pos; ?>;
 </head>
 <h1>Babo's Bao</h1>
 <body>
+<div class="game-container">
 	<div id="game">
 <?php
-		for ( $row = 0; $row < $max_row; $row++) {
-			if($row == 2) { $player = 2; }
-			if ($row == 0 || $row == 2) {
-				echo '<div class="player_'.$player.'">';
-			}	
-			echo '<div class="row r_'.$row.'">';
-			for ( $col = 0; $col < $max_col; $col++) {
-				if($row % 2) { 
-					$ply_pos--;
-				} else {
-					$ply_pos++;
-				}
-
-				echo '<div class="bin c_'.$col.' ply_'.$player.' p_'.$ply_pos.'"><span class="bin-text">'.$ply_pos.'</span>';
-				// echo '<div class="bin '.($ply_pos == $bank ? 'bank' : '').' ply_'.$player.' p_'.$ply_pos.'"><span class="bin-text">'.$ply_pos.'</span>';
-				// echo '<div class="bin '.($ply_pos == $bank ? 'bank' : '').' r_'.$row.' c_'.$col.' ply_'.$player.' p_'.$ply_pos.'"><span class="bin-text">'.$ply_pos.'</span>';
-				for($i = 0; $i < $max_bns; $i++) { echo '<div class="bean">&nbsp;</div>'; }
-				echo '</div>';
+	$pretty = 0;
+	for ( $row = 0; $row < $max_row; $row++) {
+		if($row == 2) { $player = 2; }
+		if ($row == 0 || $row == 2) {
+			if($pretty) { echo "\t\t"; }
+			echo '<div class="player_'.$player.'">';
+			if($pretty) { echo "\n"; }
+		}	
+		if($pretty) { echo "\t\t\t"; }
+		echo '<div class="row r_'.$row.'">';
+		if($pretty) { echo "\n"; }
+		for ( $col = 0; $col < $max_col; $col++) {
+			if($row % 2) { 
+				$ply_pos--;
+			} else {
+				$ply_pos++;
 			}
+
+			if($pretty) { echo "\t\t\t\t"; }
+			echo '<div class="bin c_'.$col.' ply_'.$player.' p_'.$ply_pos.'">';
+			if($pretty) { echo "\n\t\t\t\t\t"; }
+			echo '<span class="bin-text">'.$ply_pos.'</span>';
+			if($pretty) { echo "\n"; }
+			// echo '<div class="bin '.($ply_pos == $bank ? 'bank' : '').' ply_'.$player.' p_'.$ply_pos.'"><span class="bin-text">'.$ply_pos.'</span>';
+			// echo '<div class="bin '.($ply_pos == $bank ? 'bank' : '').' r_'.$row.' c_'.$col.' ply_'.$player.' p_'.$ply_pos.'"><span class="bin-text">'.$ply_pos.'</span>';
+			for($i = 0; $i < $max_bns; $i++) { if($pretty) { echo "\t\t\t\t\t"; } echo '<div class="bean">&nbsp;</div>'; if($pretty) { echo "\n"; } }
+			if($pretty) { echo "\t\t\t\t"; }
 			echo '</div>';
-			if($ply_pos == $ply_brd) { $ply_pos = (($ply_brd / 2)+1); }
-			//Lame ----v
-			if($row == 1) { $ply_pos = ($row-1); }
-			if($row == 2) { $ply_pos = (($col*2)+1); }
-			//End of lame ---^
-			//Fix this so that max_row and max_col don't need hard coded exceptions to change values
-
-
-			if ($row == 1 || $row == 3) {
-				echo '</div>';
-			}	
+			if($pretty) { echo "\n"; }
 		}
-		?>
+		if($pretty) { echo "\t\t\t"; }
+		echo '</div>';
+		if($pretty) { echo "\n"; }
+		if($ply_pos == $ply_brd) { $ply_pos = (($ply_brd / 2)+1); }
+		//Lame ----v
+		if($row == 1) { $ply_pos = ($row-1); }
+		if($row == 2) { $ply_pos = (($col*2)+1); }
+		//End of lame ---^
+		//Fix this so that max_row and max_col don't need hard coded exceptions to change values
+
+
+		if ($row == 1 || $row == 3) {
+			if($pretty) { echo "\t\t"; }
+			echo '</div>';
+				if($pretty) { echo "\n"; }
+		}	
+	}
+?>
 	</div>
+	<div id="game-stats">
+		<ul>
+			<li><span class="bank_label">Player 1 Bank: </span><input type="text" class="status-input" id="ply1_bank" size="3" readonly /></li>
+			<li><span class="bank_label">Player 2 Bank: </span><input type="text" class="status-input" id="ply2_bank" size="3" readonly /></li>
+			<li><span class="bank_label">Beans in-hand: </span><input type="text" class="status-input" id="beans_inhand" size="3" readonly /></li>
+		</ul>
+		<button id="stop-btn">Stop</button>
+		<input type="text" id="speed" value="200" size="5" />
+	</div>
+</div>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-
+speed = 200;
+orig_speed = speed;
+var loop;
+$(document).on("click", "#stop-btn", function() { clearTimeout(loop); return false; });
+$(document).on("change", "#speed", function() { speed = $("#speed").val(); });
 $(document).on("click", ".bin", function() {
 	var tclass = $(this).attr("class");
 	var player = Number(tclass.replace(/^.*ply_(\d*).*/, "$1"));
@@ -22,59 +26,81 @@ function move(position){
 function move_bean(i, beans, position) {
 	// alert("move_bean");
 	var track = i;
-	var speed = 300;
-	setTimeout(function() {
+	loop = setTimeout(function() {
 		i++;
 		console.log("move_bean(track:"+track+",i:"+i+",beans:"+beans+",position:",position);
 		if(i <= beans) {
-			var new_pos = ((position.pos+1) > 16 ? (position.pos-16+1) : (position.pos+1));
+			var new_pos = ((position.pos+1) > 16 ? (position.pos-15) : (position.pos+1));
 			var new_target = position.target.replace(/p_.*/, "p_"+new_pos).replace(/\s*\.c_(\d*)/g, '');
 			$(new_target).append('<div class="bean">&nbsp;</div>');
 			var new_position = { 
 				"player":position.player,
-				"pos":(position.pos+1),
+				"pos":(new_pos),
 				"beans":beans,
 				"col":((position.col+1) > 16 ? 1 : (position.col+1)),
 				"target":new_target
 			};
-			console.log("Target: "+new_target);
-			console.log($(new_target).html());
-			console.log("Beans: "+beans);
+			// console.log("Target: "+new_target);
+			// console.log($(new_target).html());
+			// console.log("Beans: "+beans);
 
 			if(is_valid_move(new_position, false)) { move_bean(i, beans, new_position); }
 		} else { 
 				var final_bin = $(new_target+" > .bean").length;
-				console.log("Final Bin: "+final_bin);
+				// console.log("Final Bin: "+final_bin);
 				if(is_valid_move(position, true)) { move(position); }
 		}
+		update_stats(position.player, (beans-track));
 	}, speed);
 }
+function update_stats(player, beans) {
+	//Update bank stats
+	var ply1_bank = $(".r_1 .c_4 > .bean").length;
+	var ply2_bank = $(".r_2 .c_3 > .bean").length;
+	$("#ply1_bank").val(ply1_bank);
+	$("#ply2_bank").val(ply2_bank);
+
+	//Update in-hand stats
+	$("#beans_inhand").val(beans);
+}
 function is_valid_move(position, last_move=false) {
-	console.log("is_valid_move()",position);
+	// console.log("is_valid_move()");
+	// console.log("is_valid_move()",position);
 	if(position.pos == bank && last_move ) {
-		console.log("False");
+		// console.log("False");
 		cramp(position);
 		return false;
 	}
 	console.log($(position.target+" > .bean").length);
 	if($(position.target+" > .bean").length > 1 || !last_move) { 
-		console.log("True");
+		// console.log("True");
 		return true;
 	} else { 
-		console.log("False");
+		// console.log("False");
 		cramp(position);
 		return false;
 	}
 }
 function cramp(position) { 
 	console.log("cramp()",position);
-	if(position.player === 1) { var enemy = 2; } else { var enemy = 1; }
-	var col     = ((position.col) > 16 ? (position.col-16) : (position.col));
-	var cramper = ((9-position.pos) === 4 ? 13 : (9-position.pos));
-	var target  = position.target.replace(/ply_(\d*)/, "ply_"+enemy).replace(/p_(\d*)/, "c_"+col);
+	// set_speed(2000);
+	if(position.player === 1) { var enemy = 2; 	} else { var enemy = 1; }
+	var col = ((position.pos) > 16 ? (position.pos-16) : (position.pos));
+	var crnt_pos = position.target.replace(/^.*p_(\d*).*$/, "$1");
+	// console.log("Crnt: "+crnt_pos);
+	if(crnt_pos === "5") { 
+		// console.log("Crnt: "+crnt_pos);
+		var target = position.target.replace(/ply_(\d*)/, "ply_"+enemy).replace(/p_(\d*)/, "p_13").replace(/\.c_(\d*)/, ""); 
+	} else {
+		var target  = position.target.replace(/ply_(\d*)/, "ply_"+enemy).replace(/p_(\d*)/, "p_"+(9-crnt_pos));
+		col         = $(target).attr("class").replace(/^.*c_(\d*).*$/, "$1");
+		target      = target.replace(/p_(\d*)/, "c_"+col);
+	}
+
 	var crampped_beans = $(target+" > .bean").length;
-	console.log("can cramp? "+$(".r_"+enemy+" "+target+" > .bean").length);
-	if($(".r_"+enemy+" "+target+" > .bean").length > 0) { 
+	
+	console.log("can cramp? "+target+" > .bean -> "+$(target+" > .bean").length);
+	if($(target+" > .bean").length > 0) { 
 		console.log("Cramp: "+target+"  -> "+crampped_beans);
 		$(target+" > .bean").remove();
 		move_bean(0, crampped_beans, position);
@@ -82,3 +108,4 @@ function cramp(position) {
 		return false;
 	}
 }
+function set_speed(val) { speed = val; orig_speed = val; }
